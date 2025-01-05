b821cc4a95604a30eb823d07bbff6832c243222f
ishu9805 <abhishek47903@gmail.com>
2025-01-05 23:58:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 class Config(object):
     BOT_TOKEN = "7598148687:AAEmfJIR9jwlXJkQ-_CfQQLtf209i3q41zM"
     API_ID = 22792918
-    API_HASH = "ff10095d2bb96d43d6eb7a7d9fc85f81
+    API_HASH = "ff10095d2bb96d43d6eb7a7d9fc85f81"
     DB_HOST = os.getenv("DB_HOST")
     DB_USER = os.getenv("DB_USER")
     DB_PASSWORD = os.getenv("DB_PASSWORD")
